fbc901bd38530772a9794be4b2c79fc527c1b29b
edbeh <edbeh89@gmail.com>
2020-07-01 13:28:50 +0800

    Updates

---
+++ precache-manifest.01e650d9631fc0bafd3a9a3a656d576d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0036dea894bb92a2b0c70070b20ff31",
+    "url": "/aldr-sample-1/index.html"
+  },
+  {
+    "revision": "5cb8fba26bf47c50657b",
+    "url": "/aldr-sample-1/static/css/main.7484775f.chunk.css"
+  },
+  {
+    "revision": "fc0eaf6bdf31ff76034b",
+    "url": "/aldr-sample-1/static/js/2.27728d29.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/aldr-sample-1/static/js/2.27728d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb8fba26bf47c50657b",
+    "url": "/aldr-sample-1/static/js/main.08b331c8.chunk.js"
+  },
+  {
+    "revision": "eee12e2df3df42440957",
+    "url": "/aldr-sample-1/static/js/runtime-main.d1069e38.js"
+  }
+]);
